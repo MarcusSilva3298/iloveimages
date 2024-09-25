9059d68ef0b7941b4f6baf84c439ea8b81b27cc3
marcussilva3298 <marcussilva3298@gmail.com>
2024-09-25 10:39:58 -0300

    add services module + basic AwsService

---
+++ src/Infra/Services/aws.service.ts
@@ -0,0 +1,53 @@
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Readable } from 'stream';
+import { EnvVariablesEnum } from '../../Domain/Shared/Enums/EnvVariablesEnum';
+
+@Injectable()
+export class AwsService {
+  private readonly bucketName: string;
+  private readonly s3Client: S3Client;
+
+  constructor(private readonly configService: ConfigService) {
+    this.bucketName = this.configService.get<string>(
+      EnvVariablesEnum.AWS_BUCKET_NAME,
+    );
+
+    this.s3Client = new S3Client({
+      region: this.configService.get<string>(EnvVariablesEnum.AWS_REGION),
+      credentials: {
+        accessKeyId: this.configService.get<string>(
+          EnvVariablesEnum.ACCESS_KEY_ID,
+        ),
+        secretAccessKey: this.configService.get<string>(
+          EnvVariablesEnum.ACCESS_KEY_SECRET,
+        ),
+      },
+    });
+  }
+
+  async listFiles() {
+    const listCommand = new ListObjectsV2Command({
+      Bucket: this.bucketName,
+    });
+
+    const listResponse = await this.s3Client.send(listCommand);
+
+    console.log(listResponse.Contents);
+  }
+
+  async getFile(key: string) {
+    const command = new GetObjectCommand({
+      Bucket: this.bucketName,
+      Key: key,
+    });
+
+    const response = await this.s3Client.send(command);
+    return response.Body as Readable;
+  }
+}
